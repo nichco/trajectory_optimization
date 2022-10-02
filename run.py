de1617be137601298ea52922014af2ed0daeb496
Nicholas Orndorff <norndorff@live.com>
2022-10-01 20:19:42 -0700

    works

--- run.py
+++ run.py
@@ -58,7 +58,7 @@ class RunModel(csdl.Model):
         self.add_constraint('final_altitude', lower=100)
 
         # add design variables
-        # self.add_design_variable('theta',lower=-np.pi/6,upper=np.pi/6)
+        self.add_design_variable('theta',lower=-np.pi/4,upper=np.pi/4)
         self.add_design_variable('power',lower=0, upper=2.8)
         # self.add_design_variable('dt',lower=0,upper=3)
 
@@ -80,7 +80,7 @@ sim = python_csdl_backend.Simulator(RunModel(dt=dt,mass=mass,wing_area=wing_area
 # sim.run()
 
 prob = CSDLProblem(problem_name='Trajectory Optimization', simulator=sim)
-optimizer = SLSQP(prob, maxiter=400, ftol=1e-8)
+optimizer = SLSQP(prob, maxiter=800, ftol=1e-8)
 optimizer.solve()
 optimizer.print_results()
 
