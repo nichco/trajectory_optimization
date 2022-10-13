c5563661db1bea68efd6aaa0855588a730926726
Nicholas Orndorff <norndorff@live.com>
2022-10-12 20:47:30 -0700

    edit

--- spline_explicit.py
+++ spline_explicit.py
@@ -1,7 +1,8 @@
 import csdl
 import python_csdl_backend
 import numpy as np
-from smt.surrogate_models import RMTB, RBF
+from smt.surrogate_models import RMTB, RBF, RMTC, KRG
+import matplotlib.pyplot as plt
 
 class spline(csdl.Model):
     def initialize(self):
@@ -54,14 +55,20 @@ class SplineExplicit(csdl.CustomExplicitOperation):
 
         xlimits = np.array([[0.0, num_nodes*dt]])
 
+        # sm = RBF(d0=0.1,print_global=False,print_solver=False,)
+
         sm = RMTB(
             xlimits=xlimits,
-            order=4,
-            num_ctrl_pts=20,
+            order=3,
+            num_ctrl_pts=10,
             energy_weight=1e-15,
-            regularization_weight=0.0,)
+            regularization_weight=0.0,
+            print_global=False,
+            print_solver=False,)
         sm.set_training_values(xt, yt)
         sm.train()
+
+        self.sm = sm
 
         # vector for spline interpolation
         xnew = np.arange(0, num_nodes*dt, dt)
@@ -77,23 +84,12 @@ class SplineExplicit(csdl.CustomExplicitOperation):
         num_nodes = self.parameters['num_nodes']
         dt = self.parameters['dt']
 
-        # redo surrogate generation
-        xt = np.linspace(0,num_nodes*dt,N)
-        yt = inputs['control']
-        xlimits = np.array([[0.0, num_nodes*dt]])
-        sm = RMTB(
-            xlimits=xlimits,
-            order=4,
-            num_ctrl_pts=20,
-            energy_weight=1e-15,
-            regularization_weight=0.0,)
-        sm.set_training_values(xt, yt)
-        sm.train()
 
         xnew = np.arange(0, num_nodes*dt, dt)
-        yder_dict = sm.predict_output_derivatives(xnew)
+        yder_dict = self.sm.predict_output_derivatives(xnew)
 
         array = np.array(yder_dict[None])
+        print(array)
 
         derivatives['interp', 'control'] = array
 
@@ -102,7 +98,10 @@ class SplineExplicit(csdl.CustomExplicitOperation):
 sim = python_csdl_backend.Simulator(spline(N=10,num_nodes=100,dt=0.1))
 sim.run()
 
+interp = sim['interp']
 print(sim['interp'])
+plt.plot(interp)
+plt.show()
 # print partials
 sim.check_partials(compact_print=True)
 
@@ -113,14 +112,29 @@ num_nodes=10
 dt=0.1
 xt = np.linspace(0,num_nodes*dt,N)
 yt = np.ones(N)
+yt = np.linspace(0,num_nodes*dt,N)
+xlimits = np.array([[0.0, num_nodes*dt]])
 
 sm = RBF(d0=0.1,print_global=False,print_solver=False,)
 sm.set_training_values(xt, yt)
 sm.train()
 
-xnew = np.arange(0, num_nodes*dt, dt)
-dict = sm.predict_output_derivatives(xnew)
+sm = RMTB(
+            xlimits=xlimits,
+            order=4,
+            num_ctrl_pts=20,
+            energy_weight=1e-15,
+            regularization_weight=0.0,
+            print_global=False,
+            print_solver=False,)
+sm.set_training_values(xt, yt)
+sm.train()
 
+xnew = np.arange(0, num_nodes*dt, dt)
+ynew = sm.predict_values(xnew)
+dict = sm.predict_output_derivatives(xnew)
 array = np.array(dict[None])
-print(np.shape(array))
+print(array)
+
+print(ynew)
 """
