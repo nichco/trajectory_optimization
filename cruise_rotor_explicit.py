4a53193e3a54e032d7752315f27cf1e48334a257
Nicholas Orndorff <norndorff@live.com>
2022-10-28 15:07:47 -0700

    explicit work

--- cruise_rotor_explicit.py
+++ cruise_rotor_explicit.py
@@ -8,8 +8,8 @@ class rotor(csdl.Model):
     def define(self):
         name = self.parameters['name']
 
-        vAxial = self.declare_variable('vAxial')
-        vTan = self.declare_variable('vTan')
+        vAxial = self.declare_variable(name+'vAxial')
+        vTan = self.declare_variable(name+'vTan')
         # custom operation insertion
         cruise_ct, cruise_cp = csdl.custom(vAxial,vTan, op=rotorExplicit(name=name))
         self.register_output(name+'cruise_ct', cruise_ct)
@@ -20,3 +20,17 @@ class rotorExplicit(csdl.CustomExplicitOperation):
         self.parameters.declare('name',types=str)
     def define(self):
         name = self.parameters['name']
+
+        # input: axial and tangential freestream velocities
+        self.add_input(name+'vAxial', shape=(1,))
+        self.add_input(name+'vTan', shape=(1,))
+
+        # output: thrust coefficient and power coefficient
+        self.add_output(name+'ct', shape=(1,))
+        self.add_output(name+'cp', shape=(1,))
+
+        # declare derivatives
+        self.declare_derivatives(name+'ct', name+'vAxial')
+        self.declare_derivatives(name+'ct', name+'vTan')
+        self.declare_derivatives(name+'cp', name+'vAxial')
+        self.declare_derivatives(name+'cp', name+'vTan')
